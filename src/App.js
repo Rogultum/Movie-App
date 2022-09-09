058ccc609c70bb54cc59d7479a92dd35b6848b0e
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-10 00:55:49 +0300

    add redux provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
 
 import SignStackNavigation from './navigation/SignStackNavigation/SignStackNavigation';
+import { store } from './redux/store/store';
 
 function App() {
   return (
-    <NavigationContainer>
-      <SignStackNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SignStackNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
